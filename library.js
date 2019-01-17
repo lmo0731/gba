96eb22959241cdd649a2820669cdeea1192d40d2
lmo <lmo0731@gmail.com>
2019-01-17 16:20:14 +0800

    update library

--- library.js
+++ library.js
@@ -342,7 +342,7 @@
 
           self.drive.downloadFile(file).then(function(data) {
 
-            self.store.setProperty(App.Controller.Domain.GAMES, identifier, data);
+            self.store.setProperty(App.Controller.Domain.GAMES, identifier, utilities.btoa(data));
             delete self.fetches[identifier];
             deferred.resolve(data);
 
@@ -355,7 +355,7 @@
         } else {
           self.logging.info("Using locally stored game for '" + identifier + "' with length " + data.length);
           delete self.fetches[identifier];
-          deferred.resolve(data);
+          deferred.resolve(utilities.atob(data));
         }
       });
 
